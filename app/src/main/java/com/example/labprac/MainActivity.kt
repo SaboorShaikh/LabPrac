9d93151aec1f4d2f923d98fa72b0bda39cf14827
Abdul Saboor <abdulsaboor.bscssef20@iba-suk.edu.pk>
2024-03-28 10:59:37 +0500

    Sixth commit

--- app/src/main/java/com/example/labprac/MainActivity.kt
+++ app/src/main/java/com/example/labprac/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.labprac.ui.theme.LabPracTheme
 
 class MainActivity : ComponentActivity() {
+    lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +23,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    navController = rememberNavController()
+                    SetupNavGraph(navController = navController)
                 }
             }
         }
@@ -36,10 +39,10 @@ fun GreetingPreview() {
 //    LoginScreen()
 //    Greeting()
 //    myApp()
-/*    SuperScriptText(
-        normalText = "Hello",
-        superText = "World!",
-        superTextFontWeight = FontWeight.Light
-    )*/
+    /*    SuperScriptText(
+            normalText = "Hello",
+            superText = "World!",
+            superTextFontWeight = FontWeight.Light
+        )*/
 
 }
